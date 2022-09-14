44a0cbc4fa1319de12e12d1325c3dc05a021c923
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-09-14 12:50:12 +0200

    Used Ref

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,101 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
+
+
+
+
+
+
+
+
+
+
+
+function Mailbox(props) {
+  const unreadMessages = "abc";
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  );
+}
+
+
+class CustomTextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    // create a ref to store the textInput DOM element
+    this.textInput = React.createRef();
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  focusTextInput() {
+    // Explicitly focus the text input using the raw DOM API
+    // Note: we're accessing "current" to get the DOM node
+    this.textInput.current.focus();
+  }
+
+  render() {
+    // tell React that we want to associate the <input> ref
+    // with the `textInput` that we created in the constructor
+    return (
+      <div>
+        <input
+          type="text"
+          ref={this.textInput} />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.focusTextInput}
+        />
+      </div>
+    );
+  }
+}
+
+
+
+function CustomTextInputFC(){
+
+  let textInput = React.createRef();
+  const focusTextInput =()=> {
+    // Explicitly focus the text input using the raw DOM API
+    // Note: we're accessing "current" to get the DOM node
+    textInput.current.focus();
+  }
+
+
+  return (
+    <div>
+      <input
+        type="text"
+        ref={textInput} />
+      <input
+        type="button"
+        value="Focus the text input"
+        onClick={focusTextInput}
+      />
+    </div>
+  );
+
+
+}
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CustomTextInput />
+        <CustomTextInputFC />
     </div>
   );
 }
